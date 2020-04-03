29f7f2e19921742f69ccd240e208c7379303125d
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2020-04-03 15:35:27 -0400

    added nightlife page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NightlifeComponent } from './Night/nightlife/nightlife.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'nightlife', component: NightlifeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
