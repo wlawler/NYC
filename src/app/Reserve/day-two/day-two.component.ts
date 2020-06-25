c6d3b0afb3f5a189d54fed04fbc5c730547c2666
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2020-06-25 18:45:50 -0400

    did ngStyles

--- src/app/Reserve/day-two/day-two.component.ts
+++ src/app/Reserve/day-two/day-two.component.ts
@@ -8,8 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class DayTwoComponent implements OnInit {
 
   constructor() { }
+ 
+  canSize = true ; 
+  canStretch = true; 
+  canWeight = true; 
+  ngOnInit()  {
+    this.setCurrentStyles() ;
+    
+  }
 
-  ngOnInit(): void {
+  currentStyles: {};
+  setCurrentStyles(){
+    //sets CSS Styles
+    this.currentStyles = {
+      'font-size': this.canSize  ? '8px' : '64px',
+      'font-stretch': !this.canStretch ? 'ultra-condensed' : 'ultra-expanded',
+      'font-weight': this.canWeight ? '9' : '900',
+    }
   }
 
 }
